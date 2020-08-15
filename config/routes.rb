543ea8826992ff86f0f2577f11ec6391d9baff4b
DarkGolly <geuwk65@gmail.com>
2020-08-15 22:04:02 +0500

    ФиналОчка

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'posts#index'
-  resources :posts
+
+  resources :posts, only: [:show, :index]
+  namespace :admin do
+    resources :posts, except: [:show, :index]
+
+  end
 end
